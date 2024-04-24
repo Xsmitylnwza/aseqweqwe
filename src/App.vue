9d4df9f8de735d6577b17695b9e88fc1c5cf8e80
zuloon <wongsatron.tun@gmail.com>
2024-04-24 11:58:41 +0700

    first-job

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router"
+import ListTodo from "./components/ListTodo.vue"
+</script>
+
+<template>
+	<div>
+		<div class="m-5">
+			<p>IT-Bangmod Kraddan Kanban</p>
+		</div>
+		<div>
+			<ListTodo />
+		</div>
+	</div>
+</template>
+
+<style scoped></style>
---
+++ src/components/ListTodo.vue
@@ -0,0 +1,24 @@
+<script setup></script>
+
+<template>
+	<div>
+		<div class="w-[500px] text-red-500 border border-black px-8">
+			<table class="w-[100%]">
+				<tr class="flex justify-between">
+					<th>No</th>
+					<th>Tittle</th>
+					<th>Assignees</th>
+					<th>Status</th>
+				</tr>
+				<tr class="flex justify-between">
+					<td>No</td>
+					<router-link><td>Tittle</td></router-link>
+					<td>Assignees</td>
+					<td>Status</td>
+				</tr>
+			</table>
+		</div>
+	</div>
+</template>
+
+<style scoped></style>
